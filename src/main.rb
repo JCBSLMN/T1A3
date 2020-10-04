f7b76673f7d36cfd845b5e2c4ce7d2574a2b14d1
jacob solomon <jacobsolomon6@icloud.com>
2020-10-04 10:50:34 +1000

    added clear method

--- src/main.rb
+++ src/main.rb
@@ -7,6 +7,24 @@ require "colorize"
 require "mail"
 require "tty-spinner"
 
+def anything_else
+    prompt = TTY::Prompt.new
+    else_select = prompt.select('Anything else?'.colorize(:color => :white, :background => :red), %w(yes no))
+            if else_select == 'yes'
+            end
+            if else_select == 'no'
+                puts "Good-bye".colorize(:color => :white, :background => :red)
+                loop == false
+                # break
+            end 
+end
+
+def clear
+    system('clear')
+    puts "NOTIFIER APP\n".colorize(:color => :white, :background => :red)
+
+end
+
 user_select = ''
 
 if ARGV.include? "-h"
@@ -28,10 +46,9 @@ elsif ARGV.include? "-c"
 
 elsif ARGV[0].nil?
 prompt = TTY::Prompt.new
-system('clear')
-
-user_select = prompt.select("Welcome to the biscuit, what are you?".colorize(:color => :white, :background => :red), %w(Store_user Customer_user))
-system('clear')
+clear
+user_select = prompt.select("Welcome to the biscuit, what are you?".blue, %w(Store_user Customer_user))
+clear
 end
 
 loop = true
@@ -42,24 +59,15 @@ if user_select == 'Store_user'
         prods = Csv.new("Products.csv")
         puts store_select = prompt.select("what would you like to do?".colorize(:color => :white, :background => :red), %w(View_Orders Notify_Customers Edit_Products))
         if store_select == 'View_Orders'
-            puts orders.data
-            else_select = prompt.select('Anything else?'.colorize(:color => :white, :background => :red), %w(yes no))
-            if else_select == 'yes'
-            end
-            if else_select == 'no'
-                puts "Good-bye".colorize(:color => :white, :background => :red)
-                loop == false
-                break
-            end      
+            puts orders.data  
+            anything_else
         end
 
         if store_select == 'Notify_Customers'
-            system('clear')
-            puts 'Current Products:'.colorize(:color => :white, :background => :red)
-            puts ""
+            clear
+            puts "Current Products:\n".colorize(:color => :white, :background => :red)
             puts prods.data
-            puts""
-            puts "Enter the number of the product that was delivered:".colorize(:color => :white, :background => :red)
+            puts "\nEnter the number of the product that was delivered:".colorize(:color => :white, :background => :red)
             num = STDIN.gets.chomp.to_i
             puts "Enter how many were delivered:".colorize(:color => :white, :background => :red)
             amount = STDIN.gets.chomp.to_i
@@ -74,9 +82,10 @@ if user_select == 'Store_user'
                         subject  'Order is ready'
                         body     "Your order of #{order["quantity"]} #{order["product"]} can be picked up."
                     end
+                    amount = amount - order["quantity"].to_i
                     orders.data.delete(n)
-                    amount = amount - "#{order["quantity"]}".to_i 
-                    else
+                    puts orders.data
+                else
                     n += 1
                 end 
             end         
@@ -88,27 +97,16 @@ if user_select == 'Store_user'
                 end
             end
         end
-            puts "Customers have been notified"
-            puts ""
-
-            else_select = prompt.select('Anything else?', %w(yes no))
-            if else_select == 'yes'
-            end
-            if else_select == 'no'
-                puts "Good-bye"
-                loop == false
-                break
-            end
+            puts "Customers have been notified\n"
+            anything_else
         end
 
         if store_select == 'Edit_Products'
-            system('clear')
-            puts 'Current Products:'
-            puts ""
+            clear
+            puts "Current Products:\n"
             puts prods.data
-            puts ""
 
-            edit_select = prompt.select("What would you like to do?", %w(Remove_product Add_product))
+            edit_select = prompt.select("\nWhat would you like to do?", %w(Remove_product Add_product))
 
             if edit_select == 'Add_product'
                 puts "What is the name of the product you want to add?"
@@ -128,14 +126,7 @@ if user_select == 'Store_user'
 
             
             
-            else_select = prompt.select('Anything else?', %w(yes no))
-            if else_select == 'yes'
-            end
-            if else_select == 'no'
-                puts "Good-bye."
-                loop == false
-                break
-            end
+            anything_else
         end
     end
 end
@@ -149,35 +140,22 @@ if user_select == 'Customer_user'
     while loop == true
         orders = Csv.new("Orders.csv")
         prods = Csv.new("Products.csv")
-        puts "Welcome to the biscuit factory!"
-        puts ""
-        puts "These are our current products:"
-        puts prods.data
-        puts ""
-        puts "Enter the number of the product you'd like to order:"
+        clear
+        puts "Welcome to the biscuit factory!\n\n"
+        puts "These are our current biscuits:\n\n"
+        puts prods.data 
+        puts "\nEnter the number of the biscuit you'd like to order:"
         num = STDIN.gets.chomp.to_i
-        system('clear')
-        puts "You are ordering #{prods.data[num - 1]["product"]}" 
-        puts ""
+        clear
+        puts "You are ordering #{prods.data[num - 1]["product"]} bicuits. \n\n" 
         puts "Enter the quantity you'd like to order:"
         amount = STDIN.gets.chomp
-        puts ""
-        puts "Enter the email you'd like to be notified on:"
+        puts "\nEnter the email you'd like to be notified on:"
         ph = STDIN.gets.chomp
-        system('clear')
-        puts "Your order of #{amount} #{prods.data[num - 1]["product"]} has been entered."
-        puts ""
-        puts "You will recieve a notification on #{ph} when ready."
-        puts ""
+        clear
+        puts "Your order of #{amount} #{prods.data[num - 1]["product"]} biscuits has been entered.\n"
+        puts "You will recieve a notification on #{ph} when ready.\n"
         orders.close("Orders.csv", [prods.data[num - 1]["product"],amount,ph])
-        else_select = prompt.select('Anything else?', %w(yes no))
-            if else_select == 'yes'
-            end
-            if else_select == 'no'
-                puts""
-                puts "Good-bye."
-                loop == false
-                break
-            end
-        end
+        anything_else
     end        
+end
